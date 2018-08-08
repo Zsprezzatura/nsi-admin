9338c068e1bf57687652794bc807d36e2f294e3a
dongyankun <1366455248@qq.com>
2018-08-08 14:47:33 +0800

    1

--- src/js/config.js
+++ src/js/config.js
@@ -4,11 +4,11 @@
 // var changeUrl = { address:'192.168.0.122:8080/nsi-0.9'}  //李岩CSTBY
 // var changeUrl = { address:'192.168.0.191:8080/nsi-0.9'} //罗震
 // var changeUrl = { address:'192.168.191.2:8080/nsi-0.9'} //罗震
-var changeUrl = { address:'data.xinxueshuo.cn:80/nsi-0.9',imgAddress:"data.xinxueshuo.cn"}   //服务器地址
+var changeUrl = { address:'data.xinxueshuo.cn:80/nsi-1.0',imgAddress:"data.xinxueshuo.cn"}   //服务器地址
 
 //权限管理 01-管理人员库 02-管理数据库 03-管理新学说课堂 04-超级管理员
 var rightsControl ={
-    "right01":['yanjilin@xinxueshuo.cn','info@xinxueshuo.cn','zhangwen@xinxueshuo.cn','test08','test0802','zhangziyu@xinxueshuo.cn','zhangchao@xinxueshuo.cn','taozui@xinxueshuo.cn'],
+    "right01":['qinqiaoyun@xinxueshuo.cn','yanjilin@xinxueshuo.cn','info@xinxueshuo.cn','zhangwen@xinxueshuo.cn','test08','test0802','zhangziyu@xinxueshuo.cn','zhangchao@xinxueshuo.cn','taozui@xinxueshuo.cn'],
     "right02":[],
     "right03":['zhangwen@xinxueshuo.cn','test0802'],
     "right04":['谢鹏飞','吴越','程静','chengjing@xinxueshuo.cn','wuyue@xinxueshuo.cn','xiepengfei@xinxueshuo.cn','renbinbin@xinxueshuo.cn','liyan@xinxueshuo.cn','zhangshanlin@xinxueshuo.cn','dongyankun@xinxueshuo.cn']
