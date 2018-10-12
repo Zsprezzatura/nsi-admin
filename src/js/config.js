2df431232d9d3ee885d173474f767281c6859efc
dongyankun <1366455248@qq.com>
2018-10-12 16:36:01 +0800

    新学说后台

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@
 // var changeUrl = { address:'192.168.0.159:8080/nsi-0.9'}  //李岩
 // var changeUrl = { address:'192.168.191.4:8080/nsi-0.9'}  //李岩IT
 // var changeUrl = { address:'192.168.0.122:8080/nsi-0.9'}  //李岩CSTBY
-// var changeUrl = { address:'192.168.0.49:8080/nsi-1.0'} //罗震
+ //var changeUrl = { address:'192.168.0.135:8080/nsi-1.0'} //罗震
  //var changeUrl = { address:'192.168.49.2:8080/nsi-0.9'} //罗震
 var changeUrl = { address:'data.xinxueshuo.cn:80/nsi-1.0',imgAddress:"data.xinxueshuo.cn"}   //服务器地址
 
@@ -11,6 +11,6 @@ var rightsControl ={
     "right01":['xuxinmo@xinxueshuo.cn','huangman@xinxueshuo.cn','qinqiaoyun@xinxueshuo.cn','info@xinxueshuo.cn','zhangwen@xinxueshuo.cn','zhangziyu@xinxueshuo.cn','zhangchao@xinxueshuo.cn','taozui@xinxueshuo.cn'],
     "right02":[],
     "right03":['zhangwen@xinxueshuo.cn'],
-    "right04":['吴越','程静','test03','chengjing@xinxueshuo.cn','wuyue@xinxueshuo.cn','liyan@xinxueshuo.cn','zhangshanlin@xinxueshuo.cn','dongyankun@xinxueshuo.cn','zhangwenjia@xinxueshuo.cn']
+    "right04":['吴越','程静','test03@xinxueshuo.cn','chengjing@xinxueshuo.cn','wuyue@xinxueshuo.cn','liyan@xinxueshuo.cn','zhangshanlin@xinxueshuo.cn','dongyankun@xinxueshuo.cn','zhangwenjia@xinxueshuo.cn']
 }
   
--- src/js/nsi-class/class-job.js
+++ src/js/nsi-class/class-job.js
@@ -2,7 +2,81 @@ layui.use(['table','form'], function(){
     var table = layui.table;
     table.render({
         elem: '#instiShow'
-        ,url:'http://'+changeUrl.address+'/manager/assignment/query_student_assignment.do'
+        ,url:'http://'+changeUrl.address+'/manager/assignment/query_student_assignment.do?validCode=0'
+        ,page:true
+         ,height:'full'
+        ,request: { pageName: 'pageNum' ,limitName: 'pageSize'}
+        ,limit:10
+        ,id: 'renderReload'
+        ,cols: [[
+             {field:'courseId', title: '课程ID',align:'center',fixed: 'left',}
+            ,{field:'usermail', title: '用户', align:'center'} //minWidth：局部定义当前单元格的最小宽度，layui 2.2.1 新增
+            ,{field:'assignmentContent', title: '内容', align:'center',toolbar: '#barInsti3'}//assignmentContent
+            ,{fixed: 'right',title:'操作', width:'200', align:'center', toolbar: '#barInsti'} //这里的toolbar值是模板元素的选择器
+        ]]
+    });
+    var $ = layui.$, active = {
+        getCheckData: function(){ //获取选中数据
+            var checkStatus = table.checkStatus('idTest')
+                ,data = checkStatus.data;
+            layer.alert(JSON.stringify(data));
+        }
+        ,getCheckLength: function(){ //获取选中数目
+            var checkStatus = table.checkStatus('idTest')
+                ,data = checkStatus.data;
+            layer.msg('选中了：'+ data.length + ' 个');
+        }
+        ,isAll: function(){ //验证是否全选
+            var checkStatus = table.checkStatus('idTest');
+            layer.msg(checkStatus.isAll ? '全选': '未全选')
+        }
+        //（执行搜索）数据重载
+        ,reload: function(){
+        var instiReload = $('#instiReload');
+        //执行重载
+        table.reload('renderReload', {
+            url: 'http://'+changeUrl.address+'/manager/assignment/query_student_assignment.do',
+            page: {
+                curr: 1 //重新从第 1 页开始
+            }
+            ,where: {
+                searchKey: instiReload.val(),
+            }//需要传递的参数
+
+        });
+    }
+    };
+
+    $('.instiTable .layui-btn').on('click', function(){
+        var type = $(this).data('type');
+        active[type] ? active[type].call(this) : '';
+    });
+
+    //添加回车事件
+    $('#instiReload').keydown(function (e) {
+        var curKey = e.which; //兼容火狐
+        var instiReload = $('#instiReload');
+        if(curKey == 13){
+            //执行重载
+            table.reload('renderReload', {
+                url: 'http://'+changeUrl.address+'/manager/assignment/query_student_assignment.do',
+                page: {
+                    curr: 1 //重新从第 1 页开始
+                }
+                ,where: {
+                    searchKey: instiReload.val(),
+                }//需要传递的参数
+
+            });
+        }
+    })
+
+});
+layui.use(['table','form'], function(){
+    var table = layui.table;
+    table.render({
+        elem: '#instiShow2'
+        ,url:'http://'+changeUrl.address+'/manager/assignment/query_student_assignment.do?validCode=1'
         ,page:true
          ,height:'full'
         ,request: { pageName: 'pageNum' ,limitName: 'pageSize'}
@@ -11,8 +85,8 @@ layui.use(['table','form'], function(){
         ,cols: [[
              {field:'courseId', width:'100', title: '课程ID',align:'center',fixed: 'left',}
             ,{field:'usermail', width:'250', title: '用户', align:'center'} //minWidth：局部定义当前单元格的最小宽度，layui 2.2.1 新增
-            ,{field:'assignmentContent', width:'700', title: '内容', align:'left',templet:'<span><span style="color:#409eff">点击查看详情:</span>{{d.assignmentContent}}</span>'}
-            ,{fixed: 'right',title:'操作', width:'100', align:'center', toolbar: '#barInsti'} //这里的toolbar值是模板元素的选择器
+            ,{field:'assignmentContent', width:'700', title: '内容',align:'center',toolbar: '#barInsti3'}
+            ,{fixed: 'right',title:'操作', width:'100', align:'center', toolbar: '#barInsti2'} //这里的toolbar值是模板元素的选择器
         ]]
     });
     //监听表格复选框选择
@@ -23,7 +97,7 @@ layui.use(['table','form'], function(){
     table.on('tool(insti)', function(obj){
         var data = obj.data;
         if(obj.event === 'detail'){
-        	layer.confirm('通过审核?',{icon: 3, title:'系统提示'}, function(index){
+            layer.confirm('通过审核?',{icon: 3, title:'系统提示'}, function(index){
                     $.ajax({
                         type: "get",
                         async: true,
@@ -64,13 +138,14 @@ layui.use(['table','form'], function(){
             // });
         } else if(obj.event === 'del'){
                 layer.confirm('真的删除本条数据吗',{icon: 3, title:'系统提示'}, function(index){
+                    //删除作业
                     $.ajax({
                         type: "get",
                         async: true,
                         data: {
-                            "institutionId":data.id
+                            "assignmentId":data.id
                         },//提交的参数
-                        url: 'http://' + changeUrl.address + '/manager/institution/delete.do',
+                        url: 'http://' + changeUrl.address + '/manager/assignment/remove_assignment.do',
                         contentType: 'application/json;charset=UTF-8',
                         dataType: "json",//数据类型为json  
                         success: function (msg) {
@@ -87,14 +162,21 @@ layui.use(['table','form'], function(){
                     });
                 });
         } else if(obj.event === 'edit'){
+            console.log(data)
+            layer.open({
+              type: 1,
+               maxmin: true,
+              area: ['500px', '400px'],
+              content: data.assignmentContent //这里content是一个普通的String
+            });
             // layer.alert('编辑行：<br>'+ JSON.stringify(data))
-            if(data.HavaTalent == 0){
-                layer.msg('用户没有上传附件')
-            }else {
-               var href = 'http://data.xinxueshuo.cn/upFile/talent/'+data.UserMail+data.Name+'.'+data.HavaTalent
-               $('#resumeHref').attr("href",href)
-                document.getElementById("resumeHref").click()
-            }
+            // if(data.HavaTalent == 0){
+            //     layer.msg('用户没有上传附件')
+            // }else {
+            //    var href = 'http://data.xinxueshuo.cn/upFile/talent/'+data.UserMail+data.Name+'.'+data.HavaTalent
+            //    $('#resumeHref').attr("href",href)
+            //     document.getElementById("resumeHref").click()
+            // }
         }
     });
 
@@ -155,5 +237,4 @@ layui.use(['table','form'], function(){
     })
 
 });
-
 
